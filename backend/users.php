5bf9b3979a747fcac3a9cf2c4a9d2ecb6912f53a
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-09 00:21:26 +0300

    fixed user fetching api logical bugs

--- backend/users.php
+++ backend/users.php
@@ -3,37 +3,38 @@
 include 'connection.php';
 error_reporting(0);
 
-if(isset($_POST['planet'])){
-    $planet = $_POST['planet'];
+
+if(isset($_GET['planet'])){
+    $planet = $_GET['planet'];
 }else{
-    $planet = 'Earth';
+    $planet = 'earth';
 }
 
-if(isset($_POST['gender'])){
-    $gender = $_POST['gender'];
+if(isset($_GET['gender'])){
+    $gender = $_GET['gender'];
 }else{
-    $gender = 'Tree';
+    $gender = 'Male';
 }
 
 // calculating minimum birthdate
-if(isset($_POST['min_age'])){
-    $min_age = $_POST['min_age'];
+if(isset($_GET['min_age'])){
+    $min_age = $_GET['min_age'];
     $current_date = date("Y-m-d");
-    $min_birth_date = ($current_date - $min_age)."-".date("m")."-".date("d");
+    $min_birth_date = ((int)$current_date - $min_age)."-".date("m")."-".date("d");
 }else{
     $min_birth_date = '0000-00-00';
 }
 
 // calculating maximum birthdate
-if(isset($_POST['max_age'])){
-    $max_age = $_POST['max_age'];
+if(isset($_GET['max_age'])){
+    $max_age = $_GET['max_age'];
     $current_date = date("Y-m-d");
-    $min_birth_date = ($current_date - $max_age)."-".date("m")."-".date("d");
+    $max_birth_date = ((int)$current_date - $max_age)."-".date("m")."-".date("d");
 }else{
     $max_birth_date = '3000-00-00';
 }
 
-$sql = "SELECT * FROM `users` WHERE `planet` = 'earth' AND `gender` = '$gender' AND birth_date > '$min_birth_date' AND birth_date < '$max_birth_date' ORDER BY `birth_date` DESC";
+$sql = "SELECT * FROM `users` WHERE `planet` = '$planet' AND `gender` = '$gender' AND birth_date < '$min_birth_date' AND birth_date > '$max_birth_date' ORDER BY `birth_date` DESC";
 
 $result = mysqli_query($conn, $sql);
 $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
