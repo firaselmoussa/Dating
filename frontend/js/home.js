775596af3bdf5cbf845f8ea688bfc07c17148481
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-08 17:29:17 +0300

    customized slider styles

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -3,7 +3,6 @@ const filter_dropdown = document.querySelector('.filter-dropdown');
 const filter_drop_down_btn = document.getElementById('filter_drop_down_btn');
 const apply_filter = document.getElementById('apply_filter');
 
-
 filter_drop_down_btn.addEventListener('click', ()=>{
     if(filter_dropdown.style.display != 'block'){
         filter_dropdown.style.display = 'block';
