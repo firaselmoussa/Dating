7a57882c148a7667baa99e6ba266727847c0b784
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-09 00:47:52 +0300

    implemented fetch on applied filters

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -2,6 +2,8 @@
 const filter_dropdown = document.querySelector('.filter-dropdown');
 const filter_drop_down_btn = document.getElementById('filter_drop_down_btn');
 const apply_filter = document.getElementById('apply_filter');
+const planet_filter = document.getElementById('planet_filter');
+const gender_filter = document.getElementById('gender_filter');
 
 filter_drop_down_btn.addEventListener('click', ()=>{
     if(filter_dropdown.style.display != 'block'){
@@ -11,8 +13,11 @@ filter_drop_down_btn.addEventListener('click', ()=>{
     }
 })
 
+// Apply filters
 apply_filter.addEventListener('click', ()=>{
     filter_dropdown.style.display = 'none';
+
+    fetch_filtered_users(planet_filter.value, gender_filter.value, min_age_filter.value, max_age_filter.value);
 })
 
 // age range
@@ -37,13 +42,15 @@ max_age_filter.addEventListener('input', ()=>{
 
 // FETCHING USERS
 
-let planet_var = 'earth';
-let gender_var = 'female';
-let min_age_var = 0;
-let max_age_var = 100;
+function fetch_filtered_users(planet, gender, min_age, max_age){
 
-fetch(`http://localhost/interstellar_date_server/users.php?planet=${planet_var}&gender=${gender_var}&min_age=${min_age_var}&max_age=${max_age_var}`)
-.then(response => response.json())
-.then((data)=>{
-    console.log(data)
-});
+    fetch(`http://localhost/interstellar_date_server/users.php?planet=${planet}&gender=${gender}&min_age=${min_age}&max_age=${max_age}`)
+    .then(response => response.json())
+    .then((data)=>{
+        console.log(data)
+    });
+
+}
+
+// initial fetch
+fetch_filtered_users('earth', 'male', 0, 100);
