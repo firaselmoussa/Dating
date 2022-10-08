32461e2df4500819c15d940ce97f10314858ed84
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-08 15:10:10 +0300

    implemented show & hide filter drop-down function

---
+++ frontend/js/home.js
@@ -0,0 +1,17 @@
+// filter drop-down js
+const filter_dropdown = document.querySelector('.filter-dropdown');
+const filter_drop_down_btn = document.getElementById('filter_drop_down_btn');
+const apply_filter = document.getElementById('apply_filter');
+
+
+filter_drop_down_btn.addEventListener('click', ()=>{
+    if(filter_dropdown.style.display != 'block'){
+        filter_dropdown.style.display = 'block';
+    }else{
+        filter_dropdown.style.display = 'none';
+    }
+})
+
+apply_filter.addEventListener('click', ()=>{
+    filter_dropdown.style.display = 'none';
+})
