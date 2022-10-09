855cd750e94614d6840ae40396a5f02803329eb2
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-09 12:23:01 +0300

    optimized search_user api

--- backend/search_user.php
+++ backend/search_user.php
@@ -3,20 +3,20 @@
 include 'connection.php';
 error_reporting(0);
 
+$where = ' ';
+
 if(isset($_GET['logged_user_id'])){
     $logged_user_id = $_GET['logged_user_id'];
-}else{
-    $logged_user_id = '';
+    $where .= "`id` != '$logged_user_id'";
 }
 
 if(isset($_GET['user_name'])){
     $user_name = $_GET['user_name'];
-}else{
-    $user_name = '';
+    $where .= "AND `name` LIKE '$user_name%'";
 }
 
 
-$sql = "SELECT * FROM `users` WHERE `name` LIKE '$user_name%' AND `id` != '$logged_user_id'";
+$sql = "SELECT * FROM `users` WHERE $where";
 
 $result = mysqli_query($conn, $sql);
 $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
