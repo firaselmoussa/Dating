37f2cd04e9ba383a448cd0b23f75fa52e3ca4648
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-09 09:20:57 +0300

    avoided duplication in db by liking users api

--- backend/like_user.php
+++ backend/like_user.php
@@ -1,7 +1,7 @@
 <?php
 
 include 'connection.php';
-// error_reporting(0);
+error_reporting(0);
 
 
 if(isset($_GET['user_id'])){
@@ -16,6 +16,15 @@ if(isset($_GET['liked_user_id'])){
     $liked_user_id = '';
 }
 
+// check if liked already
+$check_duplication = "SELECT `id`, `liked_user_id`, `liked_by_id` FROM `liked_users` WHERE `liked_by_id` = '$user_id' AND `liked_user_id` = '$liked_user_id';";
+
+$check_duplication_result = mysqli_query($conn, $check_duplication);
+
+$data = mysqli_fetch_all($check_duplication_result, MYSQLI_ASSOC);
+
+
+if(count($data) < 1){
 
     $sql = "INSERT INTO `liked_users`(`liked_user_id`, `liked_by_id`) VALUES ('$liked_user_id','$user_id');";
 
@@ -26,6 +35,10 @@ if(isset($_GET['liked_user_id'])){
     }else{
         echo json_encode("Something went wrong :(");
     }
+
+}else{
+    echo json_encode("Already Liked :)");
+}
 
 
 $conn->close();
