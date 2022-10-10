536abb4251b04f34cf3cbb55b6d57e6d24d237b8
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-10-10 22:50:37 +0300

    fixed 'matches' api sql

--- backend/matches.php
+++ backend/matches.php
@@ -11,7 +11,7 @@ if(isset($_GET['logged_user_id'])){
 }
 
 
-$sql = "SELECT * FROM `liked_users` INNER JOIN `users` ON users.id = liked_users.liked_user_id Having liked_by_id = (SELECT liked_user_id FROM `liked_users` Having $having LIMIT 1)";
+$sql = "SELECT * FROM `liked_users` INNER JOIN `users` ON users.id = liked_users.liked_by_id Having liked_user_id = (SELECT liked_user_id FROM `liked_users` Having $having LIMIT 1)";
 
 $result = mysqli_query($conn, $sql);
 $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
